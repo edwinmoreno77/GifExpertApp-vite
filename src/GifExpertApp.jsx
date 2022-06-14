85660c25d35bd87cde099ac18479cacd34f90cc7
Edwin Moreno <edwinmoreno77@gmail.com>
2022-06-14 16:54:21 -0400

    changes in GifExpertApp and main because AddCategory was created

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Goku']);
+
+    // const onAddCategory = () => {
+    //     setCategories(['One Punch', ...categories]);
+    // }
+
     return (
         <div>
             <h1>GifExpertApp</h1>
+            <AddCategory setCategories={setCategories} />
+            <ol>
+                {categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })
+                }
+            </ol>
         </div>
     )
 }
